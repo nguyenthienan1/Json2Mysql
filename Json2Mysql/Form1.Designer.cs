6a04b0a9cca9c3ae4e0f92a7c8f7540694b8f73f
nguyenthienan1 <nguyenthienan1401@gmail.com>
2023-11-12 08:34:52 +0700

    Initial commit

---
+++ Json2Mysql/Form1.Designer.cs
@@ -0,0 +1,149 @@
+﻿
+namespace Json2Mysql
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.richTextBox2 = new System.Windows.Forms.RichTextBox();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(750, 77);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(59, 20);
+            this.label3.TabIndex = 13;
+            this.label3.Text = "Result:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(67, 77);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(123, 20);
+            this.label2.TabIndex = 12;
+            this.label2.Text = "Paste json here:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(63, 38);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(96, 20);
+            this.label1.TabIndex = 11;
+            this.label1.Text = "Table name:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(163, 35);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(168, 26);
+            this.textBox1.TabIndex = 10;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(625, 238);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(102, 31);
+            this.button1.TabIndex = 9;
+            this.button1.Text = "Convert";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // richTextBox2
+            // 
+            this.richTextBox2.Location = new System.Drawing.Point(750, 105);
+            this.richTextBox2.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.richTextBox2.Name = "richTextBox2";
+            this.richTextBox2.ReadOnly = true;
+            this.richTextBox2.Size = new System.Drawing.Size(529, 395);
+            this.richTextBox2.TabIndex = 8;
+            this.richTextBox2.Text = "";
+            this.richTextBox2.WordWrap = false;
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(67, 105);
+            this.richTextBox1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(542, 395);
+            this.richTextBox1.TabIndex = 7;
+            this.richTextBox1.Text = "";
+            this.richTextBox1.WordWrap = false;
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(360, 38);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(162, 24);
+            this.checkBox1.TabIndex = 14;
+            this.checkBox1.Text = "INSERT IGNORE";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1343, 534);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.richTextBox2);
+            this.Controls.Add(this.richTextBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.RichTextBox richTextBox2;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.CheckBox checkBox1;
+    }
+}
+
---
+++ Json2Mysql/Form1.cs
@@ -0,0 +1,124 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Json2Mysql
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (textBox1.TextLength == 0)
+            {
+                MessageBox.Show("Please input table name!");
+                return;
+            }
+            if (richTextBox1.TextLength == 0)
+            {
+                MessageBox.Show("Please paste json text!");
+                return;
+            }
+            string sql = JsonToMysql(richTextBox1.Text, textBox1.Text);
+            if (sql != null)
+            {
+                richTextBox2.Clear();
+                richTextBox2.Text = sql;
+            }
+        }
+
+        string JsonToMysql(string JsonText, string TableName)
+        {
+            JArray rows = null;
+            try
+            {
+                rows = JsonConvert.DeserializeObject<JArray>(JsonText);
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show("Can't deserialize json, please check again. " + e.Message);
+                return null;
+            }
+
+            StringBuilder stringSql = new StringBuilder();
+            if (checkBox1.Checked)
+            {
+                stringSql.Append("INSERT IGNORE INTO " + TableName + " (");
+            } else
+            {
+                stringSql.Append("INSERT INTO " + TableName + " (");
+            }
+            
+
+            JObject firstRow = (JObject)rows.First;
+            List<KeyValuePair<string, JToken>> listFirstRow = ToList(firstRow.GetEnumerator());
+            for (int i = 0; i < listFirstRow.Count; i++)
+            {
+                KeyValuePair<string, JToken> k = listFirstRow.ElementAt(i);
+                stringSql.Append("`" + k.Key + "`");
+                if (i < listFirstRow.Count - 1)
+                {
+                    stringSql.Append(", ");
+                }
+                else
+                {
+                    stringSql.Append(")");
+                }
+            }
+            stringSql.Append(" VALUES ");
+
+            foreach (JObject row in rows)
+            {
+                stringSql.Append("\n(");
+                List<KeyValuePair<string, JToken>> listJToken = ToList(row.GetEnumerator());
+
+                for (int i = 0; i < listJToken.Count; i++)
+                {
+                    KeyValuePair<string, JToken> kJToken = listJToken.ElementAt(i);
+                    if (kJToken.Value.Type == JTokenType.String || kJToken.Value.Type == JTokenType.Array)
+                    {
+                        stringSql.Append("'" + kJToken.Value.ToString(Formatting.None) + "'");
+                    }
+                    else
+                    {
+                        stringSql.Append(kJToken.Value);
+                    }
+
+                    if (i < listJToken.Count - 1)
+                    {
+                        stringSql.Append(", ");
+                    }
+                    else
+                    {
+                        stringSql.Append("),");
+                    }
+                }
+            }
+            string sql = stringSql.Remove(stringSql.Length - 1, 1).Append(";").ToString();
+            return sql;
+        }
+
+        List<T> ToList<T>(IEnumerator<T> enumerator)
+        {
+            var list = new List<T>();
+            while (enumerator.MoveNext())
+            {
+                list.Add(enumerator.Current);
+            }
+            return list;
+        }
+    }
+}
